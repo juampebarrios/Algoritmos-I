0246d5dc2bc72024ce163960d5c9c507b200ab08
Juanpe <juanpeveiga21@gmail.com>
2023-11-07 23:11:15 -0300

    Revert "Reserva"
    This reverts commit bb5488a478e4af13fedadc3ef229c03025210dcd.

--- src/sistemaAutogestion/Sistema.java
+++ src/sistemaAutogestion/Sistema.java
@@ -5,14 +5,12 @@ import java.util.Date;
 public class Sistema implements IObligatorio {
     ListaMedico lm;
     ListaPaciente lp;
-    ListaReserva lr;
-    
+
     @Override
     public Retorno crearSistemaDeAutogestion(int maxPacientesporMedico) {
         Retorno ret = new Retorno(Retorno.Resultado.OK);
         lm = new ListaMedico();
         lp = new ListaPaciente();
-        lr = new ListaReserva();
         return ret;
     }
 
@@ -79,66 +77,17 @@ public class Sistema implements IObligatorio {
 
     @Override
     public Retorno reservaConsulta(int codMedico, int ciPaciente, Date fecha) {
-        NodoPaciente paciente = lp.obtenerPaciente(ciPaciente);
-        NodoMedico medico = lm.obtenerMedico(codMedico);
-        NodoReserva reserva = lr.obtenerReserva(codMedico, ciPaciente);
-
-        if(paciente == null){
-            return new Retorno(Retorno.Resultado.ERROR_1);
-        }
-        if(medico == null){
-            return new Retorno(Retorno.Resultado.ERROR_2);
-        }
-        if(reserva != null){
-            return new Retorno(Retorno.Resultado.ERROR_3);
-        }
-        
-        lr.agregarInicio(codMedico, ciPaciente, fecha);
-        return new Retorno(Retorno.Resultado.OK);
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public Retorno cancelarReserva(int codMedico, int ciPaciente) {
-        NodoPaciente paciente = lp.obtenerPaciente(ciPaciente);
-        NodoMedico medico = lm.obtenerMedico(codMedico);
-        NodoReserva reserva = lr.obtenerReserva(codMedico, ciPaciente);
-
-        if(paciente == null){
-            return new Retorno(Retorno.Resultado.ERROR_1);
-        }
-        if(medico == null){
-            return new Retorno(Retorno.Resultado.ERROR_2);
-        }
-        if(reserva == null){
-            return new Retorno(Retorno.Resultado.ERROR_3);
-        }
-        if(!reserva.getEstado().equals("pendiente")){
-            return new Retorno(Retorno.Resultado.ERROR_4);
-        }
-    
-        lr.borrarElemento(codMedico, ciPaciente);
-        return new Retorno(Retorno.Resultado.OK);
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public Retorno anunciaLlegada(int codMedico, int CIPaciente) {
-        Date today = new Date();
-        NodoPaciente paciente = lp.obtenerPaciente(CIPaciente);
-        NodoMedico medico = lm.obtenerMedico(codMedico);
-        
-        if(paciente == null){
-            return new Retorno(Retorno.Resultado.ERROR_1);
-        }
-        
-        NodoReserva reserva = lr.obtenerReserva(codMedico, CIPaciente);
-        if(reserva == null || (reserva != null && !reserva.getFecha().equals(today))){
-            return new Retorno(Retorno.Resultado.ERROR_2);
-        }
-        
-        System.out.println("Medico: " + medico.getNombre() + " Numero reserva: " + reserva.getId());
-        reserva.setEstado("en espera");
-        
-        return new Retorno(Retorno.Resultado.OK);
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
